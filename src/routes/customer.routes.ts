6803fe0fe4f5573d46640d7b4337cf45f1ea9756
YusufJati <yusufjana18@gmail.com>
2024-07-27 10:10:55 +0700

    Update customer routes

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -4,8 +4,14 @@ import * as postCustomerController from '../controllers/customer/customer.post';
 
 const router = Router();
 
+// GET
 router.get('/customers', getCustomerController.getAllCustomers);
 router.get('/customer/:id', getCustomerController.getOneCustomer);
+router.get('/customer/nik/:nik', getCustomerController.getOneCustomerByNik);
+router.get('/customer/email/:email', getCustomerController.getOneCustomerByEmail);
+router.get('/custoemer/name/:name', getCustomerController.getOneCustomerByName);
+
+// POST
 router.post('/customer', postCustomerController.createCustomer);
 
 export default router;
