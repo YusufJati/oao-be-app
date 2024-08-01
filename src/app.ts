57baf4e1dd80809ffee00df692fe64c659ef4bae
YusufJati <yusufjana18@gmail.com>
2024-08-01 13:15:27 +0700

    Bla

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import * as middlewares from './middlewares';
 import MessageResponse from './interfaces/MessageResponse';
 import { stat } from 'fs';
+import { PrismaClient } from '@prisma/client';
 
 import brokerRoutes from './routes/broker.routes';
 import customerRoutes from './routes/customer.routes';
@@ -17,7 +18,7 @@ import path from 'path';
 require('dotenv').config();
 
 const app = express();
-
+const prisma = new PrismaClient();
 
 app.use(morgan('dev'));
 app.use(helmet());
@@ -53,6 +54,53 @@ app.get('/hello', (req, res) => {
 app.get('/about', (req, res) => {
     res.status(200).send('About page');
 });
+
+app.post('/customerss', async (req, res) => {
+  const { email, broker_id } = req.body;
+
+  try {
+      const result = await prisma.$transaction(async (prisma) => {
+          const customer = await prisma.customer.create({
+              data: { email }
+          });
+          const customerTransaction = await prisma.customerTransaction.create({
+              data: {
+                  customer_id: customer.id,
+                  email: customer.email,
+                  broker_id
+              },
+          });
+          const getBrokerName = await prisma.broker.findFirst({
+              where: { id: broker_id },
+              select: { nama: true },
+          });
+
+          return {
+              customer,
+              customerTransaction,
+              brokerName: getBrokerName?.nama,
+          };
+      });
+
+      res.status(201).json({
+          meta: {
+              code: 201,
+              message: 'Created',
+          },
+          data: result,
+      });
+  } catch (error) {
+      console.error('Error creating customer:', error);
+      res.status(500).json({
+          meta: {
+              code: 500,
+              message: 'Error creating customer',
+          },
+          error: error,
+      });
+  }
+});
+
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
